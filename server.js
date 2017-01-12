5562f0732e8a5a74d21395c0ad533b49ed08c970
Sean Smith <sean.smith.2009@gmail.com>
2017-01-12 00:52:33 -0800

    finished styles on index.html

--- server.js
+++ server.js
@@ -21,18 +21,30 @@ app.get('/api/get-prices', (req, res) => {
 });
 // demo fund distributions:
 app.get('/api/get-funds', (req, res) => {
-	res.send(allocateFunds(orders));
+	res.json(allocateFunds(orders));
 });
 
 // actual api-routes, must post order data
 // post for prices:
 app.post('/api/post-prices/', (req, res) => {
-	res.json(printPrices(req.body));
+	try {
+		var result = printPrices(req.body);
+		res.json(result);
+	} catch(err) {
+		console.log('Post data to prices API failed.');
+		res.status(500).send();
+	};
 });
 
 // post for fund distributions:
 app.post('/api/post-funds', (req, res) => {
-	res.send({ data: allocateFunds(req.body) });
+	try {
+		var result = allocateFunds(req.body);
+		res.json(result);
+	} catch(err) {
+		console.log('Post data to funds API failed.');
+		res.status(500).send();
+	};
 });
 
 // serve homepage
